e2ce5cc653ea2646e7455b0c11191d2a13f7e380
Alexandre Ferreira <afc2c4@gmail.com>
2026-02-07 03:54:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-887241969-a0346",
+  "appId": "1:787988014300:web:c6cbfe04f31b8b16a10019",
+  "apiKey": "AIzaSyAPkLvVDkLHPV4dF7wANDrsx0K_hMDc-kU",
+  "authDomain": "studio-887241969-a0346.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "787988014300"
+};
